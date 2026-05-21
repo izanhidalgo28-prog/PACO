5f676cb5a893f32e9a6d3cd38582f9ffa71da3e8
izanhidalgo28-prog <izanhidalgo28@gmail.com>
2026-05-21 19:28:47 +0200

    Update whatsapp.js

--- api/whatsapp.js
+++ api/whatsapp.js
@@ -20,7 +20,7 @@ module.exports = async function handler(req, res) {
   if (!conversaciones[from]) conversaciones[from] = { paso: null };
   const conv = conversaciones[from];
 
-  const SHEETS_URL = https://script.google.com/macros/s/AKfycbyQDszGP0RlFR_jsjyfyvc4ZVDg7wyHaYp8qkog1Kr-Xcciq8-r0ScXRvAz0CHA1m8aGw/exec ;
+  const SHEETS_URL = https://script.google.com/macros/s/AKfycbyQDszGP0RlFR_jsjyfyvc4ZVDg7wyHaYp8qkog1Kr-Xcciq8-r0ScXRvAz0CHA1m8aGw/exec;
   const TWILIO_SID = AC5dbda67e3e4433d40118fb90a5984ec4;
   const TWILIO_TOKEN = 6276d0dab9929c69220a58adebc4ed90 ;
 
